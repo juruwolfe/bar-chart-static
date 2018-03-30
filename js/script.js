a646a4507783b74b47b3e941bb209b3adc75ea05
Wolfe <juruwolfe@gmail.com>
2018-03-30 13:19:19 -0400

    animated

--- js/script.js
+++ js/script.js
@@ -3,51 +3,28 @@ $(document).ready(function($) {
 	var data = [
 		{
 			"month":"Jan.",
-			"temp":36
+			"temp":39,
+			"sunshine":154
 		},
 		{
 			"month":"Feb.",
-			"temp":36
+			"temp":42,
+			"sunshine":171
 		},
 		{
 			"month":"March",
-			"temp":39
+			"temp":50,
+			"sunshine":213
 		},
 		{
 			"month":"April",
-			"temp":52
+			"temp":60,
+			"sunshine":237
 		},
 		{
 			"month":"May",
-			"temp":61
-		},
-		{
-			"month":"June",
-			"temp":72
-		},
-		{
-			"month":"July",
-			"temp":77
-		},
-		{
-			"month":"Aug.",
-			"temp":75
-		},
-		{
-			"month":"Sept.",
-			"temp":68
-		},
-		{
-			"month":"Oct.",
-			"temp":57
-		},
-		{
-			"month":"Nov.",
-			"temp":48
-		},
-		{
-			"month":"Dec.",
-			"temp":48
+			"temp":71,
+			"sunshine":268
 		}
 	]
 
@@ -55,22 +32,41 @@ $(document).ready(function($) {
 	
 	for (var i = 0; i < data.length; i++) {
 		var labelHtml = '<span class="label">'+data[i].month+'</span>';
-		var barHtml = '<div class="bar" data-width="'+data[i].temp+'"></div>';
+
+		var dataTemp = 'data-temp="'+data[i].temp+'"';
+		var dataSunshine = 'data-sunshine="'+(data[i].sunshine/4)+'"';
+		var barHtml = '<div class="bar" ' + dataTemp + dataSunshine + '></div>';
 
 		barchartHTML += '<div class="barchart-row">'+labelHtml+barHtml+'</div>';
 	};
 
 	$(".chart-wrapper").html(barchartHTML);
 
-	// Start a new jQuery loop through every element on the page with a class of bar. The function we'll pass as an arugment, thanks to jQuery, comes with two arguement. The first (I've called in 'index' here) tells us what number the element is on the page. The second lets us select the element itself, so I'm calling it 'element'
-	$(".bar").each(function(index, element) {
-		// Select the element and get it's data-width attribute, that we created and set in the for loop. Assign that to a variable called 'width'
-		let width = $(element).attr('data-width');
+	setTimeout(function(){
+		// Start a new jQuery loop through every element on the page with a class of bar. The function we'll pass as an arugment, thanks to jQuery, comes with two arguement. The first (I've called in 'index' here) tells us what number the element is on the page. The second lets us select the element itself, so I'm calling it 'element'
+		$(".bar").each(function(index, element) {
+			// Select the element and get it's data-width attribute, that we created and set in the for loop. Assign that to a variable called 'width'
+			let width = $(element).attr('data-temp');
+			let indexReal = index + 1;
+			// For each element, animate it's width propery to be equal to the width we just grabbed (plus a percent sign). Take one second to do so
+			$(element).animate({
+				'width':width+"%"
+			}, 200 * indexReal)
+		});
+	},250)
+	
 
-		// For each element, animate it's width propery to be equal to the width we just grabbed (plus a percent sign). Take one second to do so
-		$(element).animate({
-			'width':width+"%"
-		}, 1000)
+	$(".button").click(function(event) {
+		var btnId = $(this).attr('id');
+		$(".bar").each(function(index, element) {
+				// Select the element and get it's data-width attribute, that we created and set in the for loop. Assign that to a variable called 'width'
+				let width = $(element).attr('data-'+btnId);
+				let indexReal = index + 1;
+				// For each element, animate it's width propery to be equal to the width we just grabbed (plus a percent sign). Take one second to do so
+				$(element).animate({
+					'width':width+"%"
+				}, 100  * indexReal)
+			});
 	});
 
 });
